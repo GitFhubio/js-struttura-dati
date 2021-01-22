f83ae9ce081ebe21bee0582561b5b4fee5c64589
Fabio Petrone <fa610movic90@gmail.com>
2021-01-22 17:09:45 +0100

    ...

--- js/script.js
+++ js/script.js
@@ -1,321 +1,257 @@
-const fieldCodes =[
-  'W','U','B','R','G'
-]
-
-const cardTypes=[
-'terre','creature','incantesimi','artefatti','instantanei','stregonerie'
-]
-
-const editions={
-  'BL':{
-    edition:'boolean',
-    rarity:'blue'
-  },
-
-  'SP':{edition:'Special',
-rarity:'red'}
-}
-
-
-const cards = [{
-
-  cardName: 'Grizzly Bears',
-
-  cost: {
-    genericCostNumber: 1,
-    costFields: [ // colors array con riferimento a fieldCodes
-      fieldCodes[0],  // 'W',  - un suo riferimento
-      fieldCodes[2]   // 'B'
-    ],
-  },
-
-  picture: 'https://www.conservationnw.org/wp-content/uploads/2020/03/JSW_6993-scaled.jpg',
-  cardType: cardTypes[1],
-  cardObject: 'Bear',
-
-  editionType: editions['BL'],
-
-  description: 'Lorem ipsum',
-  story: 'Naltro Lorem Ipsum',
-
-  score: {
-    power: 2,  // filtrarlo per power
-    toughness: 2
-  }
-
-  },
-  {
-
-    cardName: 'Sviluppatore guerriero',
-
-    cost: {
-      genericCostNumber: 3,
-      costFields: [ // colors array con riferimento a fieldCodes
-        fieldCodes[2],
-        fieldCodes[3]
-      ],
+// output HTML
+$(document).ready(function () {
+  // DATI
+  const fieldCodes = ['W', 'U', 'B', 'R', 'G'];
+​
+  const cardTypes = [
+    'terre',
+    'creature',
+    'incantesimi',
+    'artefatti',
+    'instantanei',
+    'stregonerie',
+  ];
+​
+  const powerValues = [1, 2, 3, 4, 5];
+​
+  // Abbiamo creato un oggetto di oggetti, come riferimento
+  // di una edizione. Se ad esempio scrivo editions['SP']
+  // allora otterrò tutto un oggetto che descrive
+  // con più dettagli l'edizione.
+  // come oggetto di oggetti, può essere navigato solo con il for-in
+  const editions = {
+    BL: {
+      edition: 'Boolean',
+      rarity: 'blue',
     },
-
-    picture: 'https://quifinanza.it/wp-content/uploads/sites/5/2019/10/sviluppatore-web-1-1.jpg',  // da inserire immagine
-    cardType: cardTypes[1],
-    cardObject: 'Bear',
-
-    editionType: editions['BL'],
-
-    description: 'Lo sviluppatore guerriero spezza i byte in bit!',
-    story: 'Lo sviluppatore guerriero è una forma di essere umano evoluto.',
-
-    score: {
-      power: 5,  // r
-      toughness: 3
-    }
-
+​
+    SP: {
+      edition: 'Special',
+      rarity: 'red',
     },
+  };
+​
+  const cards = [
+    // carta 1
     {
-        cardName: 'Bastianich',
-        cost:{
-          genericCost:2,
-            costFields:[
-              fieldCodes[2],
-            fieldCodes[2]
-          ]
-        },
-        picture:'https://images.lacucinaitaliana.it/wp-content/uploads/2017/02/joe_bastianich.jpg',
-        cardType:cardTypes[1],
-        cardObject:'Judge',
-        editionType: editions['GG'],
-        description:'Mangia scialatielli di pollo',
-        story:'Vuoi che muoro',
-        author:'Masterchef',
-        score:{
-          power:5,
-          toughness:5
-        }
+      cardName: 'Grizzly Bears',
+​
+      cost: {
+        genericCostNumber: 1,
+        costFields: [
+          // colors array con riferimento a fieldCodes
+          fieldCodes[0], // 'W',  - un suo riferimento
+          fieldCodes[2], // 'B'
+        ],
       },
-      {
-          cardName: 'Oriente',
-          cost:{
-            genericCost:2,
-              costFields:[
-                fieldCodes[3],
-              fieldCodes[3]
-            ]
-          },
-          picture:'https://www.open.online/wp-content/uploads/2020/04/MEDIO-ORIENTE-TAG.jpg',
-          cardType:cardTypes[0],
-          cardObject:'land',
-          editionType: editions['BL'],
-          description:'La terra del sushi',
-          story:'L\'urlo di Chang terrorizza l\'occidente',
-          author:'Chang',
-          score:{ power:1,
-              toughness:2
-            }
-        },
-        {
-            cardName: 'Stasys',
-            cost:{
-              genericCost:1,
-                costFields:[
-                  fieldCodes[1],
-                fieldCodes[3]
-              ]
-            },
-            picture:'https://images.everyeye.it/img-articoli/the-legend-of-zelda-breath-of-the-wild-dungeon-abilita-crescita-dell-eroe-speciale-v4-32625-1280x16.jpg',
-            cardType:cardTypes[2],
-            cardObject:'strange',
-            editionType: editions['GG'],
-            description:'Blocca i nemici',
-            story:'Zelda Breath of the Wild',
-            author:'Nintendo',
-            score:{
-              power:1,
-              toughness:2
-            }
-          },
-          {
-              cardName: 'Vaso di Pandora',
-              cost:{
-                genericCost:1,
-                  costFields:[
-                    fieldCodes[0],
-                  fieldCodes[2]
-                ]
-              },
-              picture:'https://cultura.biografieonline.it/wp-content/uploads/2019/12/mito-di-pandora-vaso-di-pandora.jpg',
-              cardType:cardTypes[3],
-              cardObject:'Vaso',
-              editionType: editions['BL'],
-              description:'Non aprirlo',
-              story:'Se lo apri muori',
-              author:'Artigiano sconosciuto',
-              score:{
-                power:2,
-                toughness:2
-              }
-            },
-            {
-                cardName: 'Fulmine',
-                cost:{
-                  genericCost:1,
-                    costFields:[
-                      fieldCodes[2],
-                    fieldCodes[4]
-                  ]
-                },
-                picture:'https://wfprwpnressa01.blob.core.windows.net/sicurauto/2019/10/10/14/00/Fulmine.jpg',
-                cardType:cardTypes[4],
-                cardObject:'Out of nowhere',
-                editionType: editions['BL'],
-                description:'Non puoi sfuggire se hai metallo addosso',
-                story:'A ciel sereno',
-                author:'Dio',
-                score:{
-                  power:5,
-                  toughness:2
-                }
-              },
-              {
-                  cardName: 'Ascia di lava',
-                  cost:{
-                    genericCost:1,
-                      costFields:[
-                        fieldCodes[0],
-                      fieldCodes[2]
-                    ]
-                  },
-                  picture:'https://media.newyorker.com/photos/5f5908c225409f35fe11402e/1:1/w_1706,h_1706,c_limit/Clegg-OnLava.jpg',
-                  cardType:cardTypes[5],
-                  cardObject:'Ascia',
-                  editionType: editions['BL'],
-                  description:'Divide tutto a metà',
-                  story:'Ha diviso originariamente buoni e cattivi',
-                  author:'Freud',
-                  score:{
-                    power:3,
-                    toughness:2
-                  }
-                },
-                {
-                    cardName: 'Invocazione a raccolta',
-                    cost:{
-                      genericCost:1,
-                        costFields:[
-                          fieldCodes[0],
-                        fieldCodes[2]
-                      ]
-                    },
-                    picture:'https://www.guidaolistica.com/media/k2/items/cache/57df6a0f34180006582f429068c6ac21_L.jpg',
-                    cardType:cardTypes[5],
-                    cardObject:'Invocazione',
-                    editionType: editions['BL'],
-                    description:'Strimgiamci a coorte',
-                    story:'Siam pronti alla morte',
-                    author:'Carducci',
-                    score:{
-                      power:1,
-                      toughness:3
-                    }
-                  },
-                  ]
-
-// MILESTONE 1
-
-var lista=document.getElementById('lista');
-cards.forEach((item) => {
-  lista.innerHTML+=`<li>
-  ${item.cardName}
-  </li>`
-});
-const cardsContainer=document.getElementById('cards-container');
-const myselect1 =document.getElementsByClassName('cards-filter1')[0];
-const myselect2 =document.getElementsByClassName('cards-filter2')[0];
-
-
-
-// MILESTONE 2
-let power_list=[];
-cards.forEach((item) => {
-let score = item.score;
-if (!power_list.includes(score.power))
-{
- power_list.push(score.power);
-}
-
-})
- power_list=power_list.sort(function(a,b){return a - b});
-power_list.forEach((element) => {
-    myselect1.innerHTML += `
-      <option value="${element}">${element}</option>
-    `
+​
+      picture: 'images/i.png',
+      cardType: cardTypes[1],
+      cardObject: 'Bear',
+​
+      editionType: editions['BL'],
+​
+      description: 'Lorem ipsum',
+      story: 'Naltro Lorem Ipsum',
+​
+      score: {
+        power: 2, // filtrarlo per power
+        toughness: 2,
+      },
+    },
+    // carta 2
+    {
+      cardName: 'Sviluppatore guerriero',
+​
+      cost: {
+        genericCostNumber: 3,
+        costFields: [
+          // colors array con riferimento a fieldCodes
+          fieldCodes[2],
+          fieldCodes[3],
+        ],
+      },
+​
+      picture: 'images/g.png', // da inserire immagine
+      cardType: cardTypes[1],
+      cardObject: 'Bear',
+​
+      editionType: editions['BL'],
+​
+      description: 'Lo sviluppatore guerriero spezza i byte in bit!',
+      story: 'Lo sviluppatore guerriero è una forma di essere umano evoluto.',
+​
+      score: {
+        power: 2, // r
+        toughness: 3,
+      },
+    },
+    // carta 3
+    {
+      cardName: 'Mewtwo',
+​
+      cost: {
+        genericCostNumber: 6,
+        costFields: [
+          // colors array con riferimento a fieldCodes
+          fieldCodes[0],
+          fieldCodes[0],
+          fieldCodes[0],
+        ],
+      },
+​
+      picture: 'images/mewtwo.png', // da inserire immagine
+      cardType: cardTypes[1],
+      cardObject: 'Pokemon',
+​
+      editionType: editions['BL'],
+​
+      description: 'Il potere della mente più forte del mondo',
+      story: 'Il pokemon clonato dalla leggenda di Mew',
+​
+      score: {
+        power: 5,
+        toughness: 3,
+      },
+    },
+    // carta 4
+    {
+      cardName: 'Kelenvorita mascherato',
+​
+      cost: {
+        genericCostNumber: 2,
+        costFields: [
+          // colors array con riferimento a fieldCodes
+          fieldCodes[0],
+          fieldCodes[2],
+        ],
+      },
+​
+      picture: 'images/kelenvorita.png', // da inserire immagine
+      cardType: cardTypes[1],
+      cardObject: "Mezz'orco",
+​
+      editionType: editions['SP'],
+​
+      description:
+        'Quando arriva a 0 punti ferita acquista un punto ferita e invece di finire al cimitero recupera forza',
+      story: 'Kelenvor predica la grigia ed imparziale giustizia della morte',
+​
+      score: {
+        power: 4,
+        toughness: 3,
+      },
+    },
+    // carta 5
+    {
+      cardName: 'Paralyze',
+      cost: {
+        genericCostNumber: '',
+        costFields: [
+          // colors array con riferimento a fieldCodes
+          fieldCodes[2], // 'B',  - un suo riferimento
+        ],
+      },
+      picture: 'images/i.png',
+      cardType: cardTypes[2],
+      cardObject: 'Enchant',
+      editionType: editions['SP'],
+      description: 'When paralyze ecc..',
+      story: '',
+      authorString: 'autore copyright autore',
+      cardColor: fieldCodes[2],
+      score: {
+        power: 3, // filtrarlo per power
+        toughness: 0,
+      },
+    },
+    // carta 6
+    {
+      cardName: 'Dancing Scimitar',
+​
+      cost: {
+        genericCostNumber: 4,
+        costFields: [],
+      },
+​
+      picture: 'images/g.png', // da inserire immagine
+      cardType: cardTypes[3],
+      cardObject: 'Spirit',
+​
+      editionType: editions['BL'],
+​
+      description:
+        'Vola (questa creautare non può essere fermata eccetto da una creatura volante )',
+      story:
+        "Una spada che non ha mai conosciuto il fodero, un'impugnatura che non ha mai conosciuto mano",
+​
+      score: {
+        power: 1,
+        toughness: 5,
+      },
+    },
+  ];
+​
+  //METODI (LOGICA)
+  function filterByPower(powerValue, array) {
+    return array.filter((element) => {
+      return element.score.power === powerValue;
+    });
+  }
+​
+  function filterByCardType(cardTypeValue, array) {
+    return array.filter((element) => {
+      return element.cardType == cardTypeValue;
+    });
+  }
+​
+  function render(DOMElementId, array) {
+    const cardListHTMLElement = document.getElementById(DOMElementId);
+    cardListHTMLElement.innerHTML = '';
+​
+    array.forEach((element) => {
+      cardListHTMLElement.innerHTML += `<li>
+      <div>
+     <h2>Nome carta: ${element.cardName} </h2>
+     <h4>Tipo carta: ${element.cardType} </h4>
+      </div>
+      </li>`;
+    });
+  }
+​
+  function renderSelect(DOMElementId, array) {
+    const select = document.getElementById(DOMElementId);
+​
+    array.forEach((element) => {
+      select.innerHTML += `<option value="${element}"> ${element} </option>`;
+    });
+  }
+​
+  // OUTPUT
+  render('listaCarte', cards);
+  renderSelect('powerSelect', powerValues);
+  renderSelect('cardTypeSelect', cardTypes);
+​
+  //eventi da registrare
+  $('#powerSelect').change(function () {
+    if (isNaN($(this).val())) {
+      alert('Scegli un valore numerico');
+    } else {
+      const selectValue = parseInt($(this).val());
+      const filteredArray = filterByPower(selectValue, cards);
+​
+      render('listaCarte', filteredArray);
+    }
   });
-
-  myselect1.addEventListener('change',() => {
-  let selected_power = myselect1.value;
-  cardsContainer.innerHTML='';
-  console.log(selected_power);
-if (selected_power=='') {
-
-console.log(cards);
-
-cards.forEach((item) => {
-const {cardName,cost,picture,cardType,cardObject,editionType,description,story,score} = item;
-cardsContainer.innerHTML+=`
-<div style="background-image: url('${picture}');">
-<div><h4>${cardName}</h4> <div><span>${cost}</span></div></div>
-
-
-</div>
-
-`
-});
-
-}
-else{
-const selected_cards = cards.filter((item) => selected_power == item.score.power );
-console.log(selected_cards);
-selected_cards.forEach((item) => {
-const {cardName,cost,picture,cardType,cardObject,editionType,description,story,score} = item;
-
-cardsContainer.innerHTML+=`
-<div style="background-image: url('${picture}');">${cardName}</div>
-
-`
-})
-}
-});
-
-// MILESTONE3
-
-cardTypes.forEach((element) => {
-   myselect2.innerHTML += `
-     <option value="${element}">${element}</option>
-   `
- });
- myselect2.addEventListener('change',() => {
- let selected_type = myselect2.value;
-   cardsContainer.innerHTML='';
-if (selected_type=='') {
-
-  cards.forEach((item) => {
-  const {cardName,cost,picture,cardType,cardObject,editionType,description,story,score} = item;
-  cardsContainer.innerHTML+=`
-<div style="background-image: url('${picture}');">${cardName}</div>
-  `
+​
+  $('#cardTypeSelect').change(function () {
+    const selectValue = $(this).val();
+    const filteredArray = filterByCardType(selectValue, cards);
+​
+    render('listaCarte', filteredArray);
   });
-console.log(cards);
-}
-else{
-
-const selected_cards = cards.filter((item) => selected_type == item.cardType );
-console.log(selected_cards);
-selected_cards.forEach((item) => {
-const {cardName,cost,picture,cardType,cardObject,editionType,description,story,score} = item;
-cardsContainer.innerHTML+=`
-<div style="background-image: url('${picture}');">${cardName}</div>
-`
+​
+  $('#resetButton').click(function () {
+    render('listaCarte', cards);
+  });
 });
-
-}
-}
-);
